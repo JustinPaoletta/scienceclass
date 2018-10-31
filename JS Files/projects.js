d44caaaabea2e572a69af50bfb15c7e07c3992ec
JustinPaoletta <justinpaoletta@icloud.com>
2018-10-31 01:37:55 -0400

    Update projects.js

--- JS Files/projects.js
+++ JS Files/projects.js
@@ -3,29 +3,24 @@ var myProjects = [
         name: "Weather Station",
         info: "The Weather Station Consisted Of A Barometer, Anemometer, Rain Gauge, And A Weather Vane!",
         photo: "../Pictures/54601574-vector-illustration-rooster-weather-vane-black-silhouette-rooster-cock-weather-vane-symbol-icon.jpg",
-        link: "https://www.google.com/",
+        link: "",
     },
 
     {
         name: "Egg Drop",
         info: "We Built Containers To Protect Eggs From Cracking And Then Dropped Them Off The Roof!",
         photo: "../Pictures/clipart-egg-drop-8.jpg",
-<<<<<<< HEAD
+
         link: "href='https://youtu.be/1T6EReKOyT4'",
-=======
-        link: "https://www.apple.com",
->>>>>>> 6387754ba9a742357a0c55d7c658fcd38c4ab806
+
     },
 
     {
         name: "Shake Table",
         info: "We Constructed A Bunch Of Towers And Shook Their Platform To See How They Survived!",
         photo: "../Pictures/maxresdefault-1.jpg",
-<<<<<<< HEAD
         link: "href='https://youtu.be/zq7DgkZk-kQ'",
-=======
-        link: "https://9to5mac.com",
->>>>>>> 6387754ba9a742357a0c55d7c658fcd38c4ab806
+
     },
 
     {
