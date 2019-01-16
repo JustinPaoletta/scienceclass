6ba151a735919c007a6a31f4f69cee98340a48ce
JustinPaoletta <justinpaoletta@icloud.com>
2019-01-16 11:38:48 -0500

    fix script

--- JS Files/vocab1.js
+++ JS Files/vocab1.js
@@ -9,161 +9,147 @@
 
   document.getElementById( 'side-1' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-2' ).className = 'flip flip-side-1';
     document.getElementById( 'side-1' ).className = 'flip flip-side-2';
     document.getElementById("side-2").style.backgroundColor = "white";
 
 
-}, false );
+});
 
 document.getElementById( 'side-2' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
+
     document.getElementById( 'side-1' ).className = 'flip flip-side-1';
     document.getElementById( 'side-2' ).className = 'flip flip-side-2';
 
-}, false );
+});
 
 
 // Card 2
 
 document.getElementById( 'side-3' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-4' ).className = 'flip flip-side-1';
     document.getElementById( 'side-3' ).className = 'flip flip-side-2';
     document.getElementById("side-4").style.backgroundColor = "white";
 
-}, false );
+});
 
 document.getElementById( 'side-4' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-3' ).className = 'flip flip-side-3';
     document.getElementById( 'side-4' ).className = 'flip flip-side-4';
 
-}, false );
+});
 
 // Card 3
 
 
 document.getElementById( 'side-5' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
+   
     document.getElementById( 'side-6' ).className = 'flip flip-side-5';
     document.getElementById( 'side-5' ).className = 'flip flip-side-6';
     document.getElementById("side-6").style.backgroundColor = "white";
 
-}, false );
+});
 
 document.getElementById( 'side-6' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-5' ).className = 'flip flip-side-5';
     document.getElementById( 'side-6' ).className = 'flip flip-side-6';
 
-}, false );
+});
 
 
 // Card 4
 
 document.getElementById( 'side-7' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-8' ).className = 'flip flip-side-7';
     document.getElementById( 'side-7' ).className = 'flip flip-side-8';
     document.getElementById("side-8").style.backgroundColor = "white";
 
-}, false );
+});
 
 document.getElementById( 'side-8' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-7' ).className = 'flip flip-side-7';
     document.getElementById( 'side-8' ).className = 'flip flip-side-8';
 
-}, false );
+});
 
 
 // Card5
 
 document.getElementById( 'side-9' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-10' ).className = 'flip flip-side-9';
     document.getElementById( 'side-9' ).className = 'flip flip-side-10';
     document.getElementById("side-10").style.backgroundColor = "white";
 
-}, false );
+});
 
 document.getElementById( 'side-10' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-9' ).className = 'flip flip-side-9';
     document.getElementById( 'side-10' ).className = 'flip flip-side-10';
 
-}, false );
+});
 
 
 // Card 6
 
 document.getElementById( 'side-11' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-12' ).className = 'flip flip-side-11';
     document.getElementById( 'side-11' ).className = 'flip flip-side-12';
     document.getElementById("side-12").style.backgroundColor = "white";
 
-}, false );
+});
 
 document.getElementById( 'side-12' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-11' ).className = 'flip flip-side-11';
     document.getElementById( 'side-12' ).className = 'flip flip-side-12';
 
-}, false );
+});
 
 // Card 7
 
 
 document.getElementById( 'side-13' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-14' ).className = 'flip flip-side-13';
     document.getElementById( 'side-13' ).className = 'flip flip-side-14';
     document.getElementById("side-14").style.backgroundColor = "white";
 
-}, false );
+});
 
 document.getElementById( 'side-14' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-13' ).className = 'flip flip-side-13';
     document.getElementById( 'side-14' ).className = 'flip flip-side-14';
 
-}, false );
+});
 
 
 // Card 8
 
 document.getElementById( 'side-15' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-16' ).className = 'flip flip-side-15';
     document.getElementById( 'side-15' ).className = 'flip flip-side-16';
     document.getElementById("side-16").style.backgroundColor = "white";
    
-}, false );
+});
 
 document.getElementById( 'side-16' ).addEventListener( 'click', function( event ) {
 
-    event.preventDefault();
     document.getElementById( 'side-15' ).className = 'flip flip-side-15';
     document.getElementById( 'side-16' ).className = 'flip flip-side-16';
 
-}, false );
+});
 
 
 
