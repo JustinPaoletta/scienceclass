a23c2b57ff367ba1cf3126bf2e2d66741dd5ec69
JustinPaoletta <justinpaoletta@icloud.com>
2019-01-16 02:57:41 -0500

    vocab change

--- JS Files/vocab1.js
+++ JS Files/vocab1.js
@@ -1,101 +1,169 @@
-// These Are FlashCards That Change The Inner Html
+// These Are FlashCards
 
-document.getElementById('mag-one').addEventListener('click', function() {
 
-if(document.getElementById('mag-one-text').innerHTML === 'A piece of iron or steel that attracts certain metals.') {
-    document.getElementById('mag-one-text').innerHTML = 'MAGNET'
-} else {
-    document.getElementById('mag-one-text').innerHTML = 'A piece of iron or steel that attracts certain metals.'
-}
-});
+// Flip Cards
 
-document.getElementById('mag2').addEventListener('click', function() {
-    if(document.getElementById('mag2text').innerHTML === 'To pull close.') {
-        document.getElementById('mag2text').innerHTML = 'ATTRACT'
-    } else {
-        document.getElementById('mag2text').innerHTML = 'To pull close.'
-    }
-});
 
-document.getElementById('mag3').addEventListener('click', function() {
-    if(document.getElementById('mag3text').innerHTML === 'To push away.') {
-        document.getElementById('mag3text').innerHTML = 'REPEL'
-    } else {
-        document.getElementById('mag3text').innerHTML = 'To push away.'
-    }
-});
 
-document.getElementById('mag4').addEventListener('click', function() {
-    if(document.getElementById('mag4text').innerHTML === 'The strongest point on a magnet.') {
-        document.getElementById('mag4text').innerHTML = 'MAGNETIC POLE'
-    } else {
-        document.getElementById('mag4text').innerHTML = 'The strongest point on a magnet.'
-    }
-});
+// Card1
 
-document.getElementById('mag5').addEventListener('click', function() {
-    if(document.getElementById('mag5text').innerHTML === 'Something that is attracted to a magnet.') {
-        document.getElementById('mag5text').innerHTML = 'MAGNETIC OBJECTS'
-    } else {
-        document.getElementById('mag5text').innerHTML = 'Something that is attracted to a magnet.';
-    }
-});
+  document.getElementById( 'side-1' ).addEventListener( 'click', function( event ) {
 
-document.getElementById('mag6').addEventListener('click', function() {
-    if(document.getElementById('mag6text').innerHTML === 'Energy that can cause something to be pushed or pulled.') {
-        document.getElementById('mag6text').innerHTML = 'FORCE'
-    } else {
-        document.getElementById('mag6text').innerHTML = 'Energy that can cause something to be pushed or pulled.';
-    }
-});
+    event.preventDefault();
+    document.getElementById( 'side-2' ).className = 'flip flip-side-1';
+    document.getElementById( 'side-1' ).className = 'flip flip-side-2';
+    document.getElementById("side-2").style.backgroundColor = "white";
 
-document.getElementById('mag7').addEventListener('click', function() {
-    if(document.getElementById('mag7text').innerHTML === 'To take away the magnetic ability of a magnet.') {
-        document.getElementById('mag7text').innerHTML = 'DE-MAGNETIZE'
-    } else {
-        document.getElementById('mag7text').innerHTML = 'To take away the magnetic ability of a magnet.';
-    }
-});
 
-document.getElementById('mag8').addEventListener('click', function() {
-    if(document.getElementById('mag8text').innerHTML === 'An instrument with a needle that points north and is used to determine direction.') {
-        document.getElementById('mag8text').innerHTML = 'COMPASS'
-    } else {
-        document.getElementById('mag8text').innerHTML = 'An instrument with a needle that points north and is used to determine direction.'
-    }
-});
+}, false );
 
-$('#mag-one').click(function() {
-    $('#mag-one').toggleClass('changetxtsize');
-  });
+document.getElementById( 'side-2' ).addEventListener( 'click', function( event ) {
 
-  $('#mag2').click(function() {
-    $('#mag2').toggleClass('changetxtsize');
-  });
+    event.preventDefault();
+    document.getElementById( 'side-1' ).className = 'flip flip-side-1';
+    document.getElementById( 'side-2' ).className = 'flip flip-side-2';
 
-  $('#mag3').click(function() {
-    $('#mag3').toggleClass('changetxtsize');
-  });
+}, false );
 
-  $('#mag4').click(function() {
-    $('#mag4').toggleClass('changetxtsize');
-  });
 
-  $('#mag5').click(function() {
-    $('#mag5').toggleClass('changetxtsize');
-  });
+// Card 2
 
-  $('#mag6').click(function() {
-    $('#mag6').toggleClass('changetxtsize');
-  });
+document.getElementById( 'side-3' ).addEventListener( 'click', function( event ) {
 
-  $('#mag7').click(function() {
-    $('#mag7').toggleClass('changetxtsize');
-  });
+    event.preventDefault();
+    document.getElementById( 'side-4' ).className = 'flip flip-side-1';
+    document.getElementById( 'side-3' ).className = 'flip flip-side-2';
+    document.getElementById("side-4").style.backgroundColor = "white";
 
-  $('#mag8').click(function() {
-    $('#mag8').toggleClass('changetxtsize');
-  });
+}, false );
+
+document.getElementById( 'side-4' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-3' ).className = 'flip flip-side-3';
+    document.getElementById( 'side-4' ).className = 'flip flip-side-4';
+
+}, false );
+
+// Card 3
+
+
+document.getElementById( 'side-5' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-6' ).className = 'flip flip-side-5';
+    document.getElementById( 'side-5' ).className = 'flip flip-side-6';
+    document.getElementById("side-6").style.backgroundColor = "white";
+
+}, false );
+
+document.getElementById( 'side-6' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-5' ).className = 'flip flip-side-5';
+    document.getElementById( 'side-6' ).className = 'flip flip-side-6';
+
+}, false );
+
+
+// Card 4
+
+document.getElementById( 'side-7' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-8' ).className = 'flip flip-side-7';
+    document.getElementById( 'side-7' ).className = 'flip flip-side-8';
+    document.getElementById("side-8").style.backgroundColor = "white";
+
+}, false );
+
+document.getElementById( 'side-8' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-7' ).className = 'flip flip-side-7';
+    document.getElementById( 'side-8' ).className = 'flip flip-side-8';
+
+}, false );
+
+
+// Card5
+
+document.getElementById( 'side-9' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-10' ).className = 'flip flip-side-9';
+    document.getElementById( 'side-9' ).className = 'flip flip-side-10';
+    document.getElementById("side-10").style.backgroundColor = "white";
+
+}, false );
+
+document.getElementById( 'side-10' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-9' ).className = 'flip flip-side-9';
+    document.getElementById( 'side-10' ).className = 'flip flip-side-10';
+
+}, false );
+
+
+// Card 6
+
+document.getElementById( 'side-11' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-12' ).className = 'flip flip-side-11';
+    document.getElementById( 'side-11' ).className = 'flip flip-side-12';
+    document.getElementById("side-12").style.backgroundColor = "white";
+
+}, false );
+
+document.getElementById( 'side-12' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-11' ).className = 'flip flip-side-11';
+    document.getElementById( 'side-12' ).className = 'flip flip-side-12';
+
+}, false );
+
+// Card 7
+
+
+document.getElementById( 'side-13' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-14' ).className = 'flip flip-side-13';
+    document.getElementById( 'side-13' ).className = 'flip flip-side-14';
+    document.getElementById("side-14").style.backgroundColor = "white";
+
+}, false );
+
+document.getElementById( 'side-14' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-13' ).className = 'flip flip-side-13';
+    document.getElementById( 'side-14' ).className = 'flip flip-side-14';
+
+}, false );
+
+
+// Card 8
+
+document.getElementById( 'side-15' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-16' ).className = 'flip flip-side-15';
+    document.getElementById( 'side-15' ).className = 'flip flip-side-16';
+    document.getElementById("side-16").style.backgroundColor = "white";
+   
+}, false );
+
+document.getElementById( 'side-16' ).addEventListener( 'click', function( event ) {
+
+    event.preventDefault();
+    document.getElementById( 'side-15' ).className = 'flip flip-side-15';
+    document.getElementById( 'side-16' ).className = 'flip flip-side-16';
+
+}, false );
 
 
 
