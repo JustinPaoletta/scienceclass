a5f16f6c3e84d9ba49dfabcce48c27fbb2ce9739
JustinPaoletta <justinpaoletta@icloud.com>
2019-01-16 22:53:15 -0500

    vocab

--- JS Files/vocab1.js
+++ JS Files/vocab1.js
@@ -3,14 +3,30 @@
 
 // Flip Cards
 
-$("#card").flip();
-$("#card2").flip();
-$("#card3").flip();
-$("#card4").flip();
-$("#card5").flip();
-$("#card6").flip();
-$("#card7").flip();
-$("#card8").flip();
+$("#card").flip({ 
+    speed: 3000
+       });
+$("#card2").flip({ 
+    speed: 3000
+       });
+$("#card3").flip({ 
+    speed: 3000
+       });
+$("#card4").flip({ 
+    speed: 3000
+       });
+$("#card5").flip({ 
+    speed: 3000
+       });
+$("#card6").flip({ 
+    speed: 3000
+       });
+$("#card7").flip({ 
+    speed: 3000
+       });
+$("#card8").flip({ 
+    speed: 3000
+       });
 
 
 
