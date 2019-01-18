cb0a020e881365cb1aac673899f67c37ee9ac375
JustinPaoletta <justinpaoletta@icloud.com>
2019-01-17 19:51:07 -0500

    done for day

--- JS Files/vocab.js
+++ JS Files/vocab.js
@@ -7,3 +7,88 @@ alert
 });
 }
 
+document.getElementById('magnet').addEventListener('click', function(){
+
+    var pixies = 0;
+
+    setInterval(function() {
+        document.getElementById('cardOne').style.position = 'relative';
+        pixies += 3.5;
+        var pixString = pixies.toString() + 'px';
+        document.getElementById('cardOne').style.bottom = pixString;
+    }, 40)
+    setTimeout(function(){
+        window.location.assign('vocabd1.html');
+    }, 1300)
+
+    
+});
+
+document.getElementById('saturn').addEventListener('click', function(){
+
+    var pixies = 0;
+
+    setInterval(function() {
+        document.getElementById('cardTwo').style.position = 'relative';
+        pixies += 3.5;
+        var pixString = pixies.toString() + 'px';
+        document.getElementById('cardTwo').style.bottom = pixString;
+    }, 40)
+    setTimeout(function(){
+        window.location.assign('vocabd2.html');
+    }, 1300)
+
+    
+});
+
+document.getElementById('chem').addEventListener('click', function(){
+
+    var pixies = 0;
+
+    setInterval(function() {
+        document.getElementById('cardThree').style.position = 'relative';
+        pixies += 3.5;
+        var pixString = pixies.toString() + 'px';
+        document.getElementById('cardThree').style.bottom = pixString;
+    }, 40)
+    setTimeout(function(){
+        window.location.assign('vocabd3.html');
+    }, 1300)
+
+    
+});
+
+document.getElementById('geo').addEventListener('click', function(){
+
+    var pixies = 0;
+
+    setInterval(function() {
+        document.getElementById('cardFour').style.position = 'relative';
+        pixies += 3.5;
+        var pixString = pixies.toString() + 'px';
+        document.getElementById('cardFour').style.bottom = pixString;
+    }, 40)
+    setTimeout(function(){
+        window.location.assign('vocabd4.html');
+    }, 1300)
+
+    
+});
+
+document.getElementById('seasons').addEventListener('click', function(){
+
+    var pixies = 0;
+
+    setInterval(function() {
+        document.getElementById('cardFive').style.position = 'relative';
+        pixies += 3.5;
+        var pixString = pixies.toString() + 'px';
+        document.getElementById('cardFive').style.bottom = pixString;
+    }, 40)
+    setTimeout(function(){
+        window.location.assign('vocabd5.html');
+    }, 1300)
+
+    
+});
+
